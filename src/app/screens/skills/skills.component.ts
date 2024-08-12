338ac1a494387d85eeaf3407c51ada85b1c043b9
Mani <mani@cortiqahealth.com>
2024-08-12 14:34:14 +0530

    skills page development done

--- src/app/screens/skills/skills.component.ts
+++ src/app/screens/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -14,23 +14,26 @@ export class SkillsComponent {
 
   ngOnInit() {
       const skill = [
-          {title:'javascript' , percent:10},
-          {title:'node' , percent: 20},
-          {title:'angular' , percent: 40},
-          {title: 'react' , percent: 60},
-          {title: 'redux' , percent: 60},
-          {title:'nest' , percent: 60},
-          {title:'sql' , percent: 80},
-          {title: 'nosql' , percent: 80}
+          {title:'javascript' , percent:'95%'},
+          {title:'node' , percent: '90%'},
+          {title:'angular' , percent: '90%'},
+          {title: 'react' , percent: '95%'},
+          {title: 'redux' , percent: '95%'},
+          {title:'nest' , percent: '90%'},
+          {title:'sql' , percent: '85%'},
+          {title: 'nosql' , percent: '80%'}
       ] as IOSkillsList[];
 
-      this.skills.push(...skill)
+      this.skills.push(...skill);
+
   }
+
+
 }
 
 
 
 export interface IOSkillsList {
     title:string;
-    percent:number
+    percent:string
 }
